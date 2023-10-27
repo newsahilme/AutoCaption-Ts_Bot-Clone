0a84632ec50fcabe6523eaff2586fea66d45d77d
newsahilme <147787937+newsahilme@users.noreply.github.com>
2023-10-27 22:27:52 +0530

    config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6570374387:AAEbb8O7StFdzGBrawhc1vqvcDBtLdmdZ7E")
+      API_ID = int(os.environ.get("APP_ID", 26852980))
       API_HASH = os.environ.get("API_HASH")
-      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
-      ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
-      DB_URL = os.environ.get("DATABASE_URL", "")
+      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "bottom")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ayurvedastudy1")
+      ADMIN_ID = int(os.environ.get("ADMIN_ID", 6049998748 )) 
+      DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://syedamanhusain:<syedamanhusain>@cluster0.k009uml.mongodb.net/?retryWrites=true&w=majority")
